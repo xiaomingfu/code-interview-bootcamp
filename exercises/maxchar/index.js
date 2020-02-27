324b5a3d0e79d2030b45ce7f1ab9e5a3a901cdc7
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-26 22:07:02 -0800

    solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // solution 1
+  //   const charMap = {};
+  //   for (let char of str) {
+  //     charMap[char] = charMap[char] + 1 || 1;
+  //   }
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
