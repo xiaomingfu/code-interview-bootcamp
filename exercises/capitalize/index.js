883fdf48870cba63683c6bae9b76524ac5a61041
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-28 00:28:45 -0800

    add second solution for capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,13 +8,28 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-  let arr = str.split(" ");
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    newArr.push(arr[i][0].toUpperCase() + arr[i].slice(1));
+  //   let words = [];
+  //   for (word of str.split(" ")) {
+  //     words.push(word[0].toUpperCase() + word.slice(1));
+  //   }
+  //   return words.join(" ");
+  //   solution 2
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
   }
+  return result;
+  //   let arr = str.split(" ");
+  //   let words = [];
+  //   for (let i = 0; i < arr.length; i++) {
+  //     words.push(arr[i][0].toUpperCase() + arr[i].slice(1));
+  //   }
 
-  return newArr.join(" ");
+  //   return words.join(" ");
 }
 
 module.exports = capitalize;
