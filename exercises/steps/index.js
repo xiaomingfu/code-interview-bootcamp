2bf1d9bfbfcd414b91a45c676baca9f61c1855f4
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-28 01:25:43 -0800

    add second solution for step

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -18,9 +18,21 @@
 //       '####'
 
 function steps(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log("#".repeat(i) + " ".repeat(n - i));
+  for (let row = 0; row < n; row++) {
+    let res = "";
+    for (let cell = 0; cell < n; cell++) {
+      if (cell <= row) {
+        res += "#";
+      } else {
+        res += " ";
+      }
+    }
+    console.log(res);
   }
+  // solution 1
+  //   for (let i = 1; i <= n; i++) {
+  //     console.log("#".repeat(i) + " ".repeat(n - i));
+  //   }
 }
 
 module.exports = steps;
