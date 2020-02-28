27a54b91c1b9a1c7d172790fed56582cc4d24304
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-28 15:29:49 -0800

    use recursive to solve steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -16,23 +16,39 @@
 //       '##  '
 //       '### '
 //       '####'
-
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let res = "";
-    for (let cell = 0; cell < n; cell++) {
-      if (cell <= row) {
-        res += "#";
-      } else {
-        res += " ";
-      }
-    }
-    console.log(res);
+function steps(n, row = 0, cell = "") {
+  if (n === row) {
+    return;
   }
-  // solution 1
-  //   for (let i = 1; i <= n; i++) {
-  //     console.log("#".repeat(i) + " ".repeat(n - i));
-  //   }
+  if (n === cell.length) {
+    console.log(cell);
+    return steps(n, row + 1);
+  }
+  //   const add = cell.length<=row ? cell+="#" : cell+=" "
+  if (cell.length <= row) {
+    cell += "#";
+  } else {
+    cell += " ";
+  }
+  steps(n, row, cell);
 }
+// solution 2
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let res = "";
+//     for (let cell = 0; cell < n; cell++) {
+//       if (cell <= row) {
+//         res += "#";
+//       } else {
+//         res += " ";
+//       }
+//     }
+//     console.log(res);
+//   }
+// solution 1
+//   for (let i = 1; i <= n; i++) {
+//     console.log("#".repeat(i) + " ".repeat(n - i));
+//   }
+// }
 
 module.exports = steps;
