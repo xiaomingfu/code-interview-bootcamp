9185063bbb6164ebe7f77788cd687fee6daa907c
Xiaoming Fu <xmingfu86@gmail.com>
2020-02-29 10:49:30 -0800

    solve pyramid by recursive

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,19 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  for (var row = 0; row < n; row++) {
-    let result = "";
-    for (var col = 0; col < 2 * n - 1; col++) {
-      const midpoint = Math.floor((2 * n - 1) / 2);
-      if (midpoint - row <= col && midpoint + row >= col) {
-        result += "#";
-      } else {
-        result += " ";
-      }
-    }
-    console.log(result);
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
   }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  //   let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+  return pyramid(n, row, level);
 }
 
 module.exports = pyramid;
+
+// solution 1
+// function pyramid(n) {
+//     for (var row = 0; row < n; row++) {
+//       let result = "";
+//       for (var col = 0; col < 2 * n - 1; col++) {
+//         const midpoint = Math.floor((2 * n - 1) / 2);
+//         if (midpoint - row <= col && midpoint + row >= col) {
+//           result += "#";
+//         } else {
+//           result += " ";
+//         }
+//       }
+//       console.log(result);
+//     }
+//   }
