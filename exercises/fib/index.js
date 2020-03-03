b62b01bd14fcd552a403c122228eff02c35de9e2
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-03 15:40:52 -0800

    solve fibonacci by recursive

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,18 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+
+  while (n >= 2) {
+    return fib(n - 1) + fib(n - 2);
+    n--;
+  }
+}
 
 module.exports = fib;
