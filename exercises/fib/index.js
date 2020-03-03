9db5623bd7d27b2bf2349ab46e0c391438c3ef9b
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-03 15:54:10 -0800

    refactor fibonacci by recursive

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,22 +9,17 @@
 //   fib(4) === 3
 
 function fib(n) {
-  // iterative (o)n
-  const result = [0, 1];
-
-  for (let i = 2; i <= n; i++) {
-    const a = result[i - 1];
-    const b = result[i - 2];
-    result.push(a + b);
+  //   recursive (O)n^2 time complexity
+  // if (n === 0) {
+  //   return 0;
+  // }
+  // if (n === 1) {
+  //   return 1;
+  // }
+  if (n <= 1) {
+    return n;
   }
-  return result[n];
-  // recursive (O)n^2 time complexity
-  //   if (n === 0) {
-  //     return 0;
-  //   }
-  //   if (n === 1) {
-  //     return 1;
-  //   }
+  return fib(n - 1) + fib(n - 2);
   //   while (n >= 2) {
   //     return fib(n - 1) + fib(n - 2);
   //     n--;
@@ -32,3 +27,12 @@ function fib(n) {
 }
 
 module.exports = fib;
+// iterative (o)n
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//   return result[n];
