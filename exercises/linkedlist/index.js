0ea9915f4125db519a145cc795f386f0f675d272
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-04 23:04:54 -0800

    solve size method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,6 +13,19 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let count = 0;
+    while (this.head) {
+      count++;
+      this.head = this.head.next;
+    }
+    return count;
+  }
 }
 
 module.exports = { Node, LinkedList };
