fd7d74ad01b2af93f1cdd6f3e7e4035d394b3d11
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-14 01:04:24 -0700

    solve selectionSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,7 +3,7 @@
 
 function bubbleSort(arr) {
   for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr.length - i; j++) {
+    for (var j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -14,7 +14,19 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      let smallest = arr[i];
+      if (arr[j] < smallest) {
+        smallest = arr[j];
+        arr[j] = arr[i];
+        arr[i] = smallest;
+      }
+    }
+  }
+  return arr;
+}
 
 function mergeSort(arr) {}
 
