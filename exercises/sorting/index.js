273993f80f1e61412fe02bc3e47a46e33cc6d425
Xiaoming Fu <xmingfu86@gmail.com>
2020-03-14 00:42:32 -0700

    solve bubbleSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,22 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
